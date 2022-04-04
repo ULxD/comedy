135d95b7ba07f598371826f9290c0582cb2b85b1
UL xD <whelidias@hotmail.com>
2022-04-04 18:35:23 -0300

    First commit

---
+++ src/components/header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 8rem;
+    width: 100%;
+    background: red;
+
+
+    .container-box{
+        width: 100%;
+        height: 100%;
+        max-width: 85rem;
+        background: green;
+        margin: 0 auto;
+
+        
+    }
+`
